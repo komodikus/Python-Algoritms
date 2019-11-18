d3802c391a4306cd9c9bd0b9d8a572ea23ad67d2
Komodo <34374256+komodikus@users.noreply.github.com>
2019-11-18 14:34:12 +0300

    Create japan_flag.py

---
+++ japan_flag.py
@@ -0,0 +1,12 @@
+def generate_japan_flag(n):
+    canvas = list()
+    canvas.append("#" * (n * 3 + 2))
+    for i in range(n // 2):
+        canvas.append("#" + ''.rjust(3 * n, "-") + "#")
+    for i in range(n // 2):
+        canvas.append("#" + f'*{"o" * i}{"o" * i}*'.center(3 * n, "-") + "#")
+    return "\n".join(canvas + canvas[::-1])
+
+
+if __name__ == '__main__':
+    print(generate_japan_flag(12))
